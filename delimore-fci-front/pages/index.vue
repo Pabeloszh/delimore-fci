e71e41d488138560e24403cdaf405e92fd8aca0a
Pabeloszh <Pabeloszh@gmail.com>
2021-09-29 13:30:04 +0200

    main header to main page added

--- delimore-fci-front/pages/index.vue
+++ delimore-fci-front/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Navbar/>
+    <MainHeader/>
     <Content/>
   </div>
 </template>
@@ -9,6 +10,8 @@
 export default {}
 </script>
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap');
   *{
     box-sizing: border-box;
     margin:0;
