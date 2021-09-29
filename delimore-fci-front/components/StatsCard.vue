7bd7ad267389415283a0fe1cc92dd41f3f6267c4
Pabeloszh <Pabeloszh@gmail.com>
2021-09-29 14:35:37 +0200

    stats added

---
+++ delimore-fci-front/components/StatsCard.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="stats-card">
+        <div class="icon">
+            <font-awesome-icon :icon="['fas', 'dog']"/>
+        </div>
+        <h3>49</h3>
+        <p>dogs under our care</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .stats-card{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        transition: transform 150ms linear;
+        width:330px;
+        padding:20px;
+        box-shadow: 0px 11px 15px 4px #00000040;
+        background: $primary-color;
+        cursor:pointer;
+        .icon{
+            position:relative;
+            margin-bottom:20px;
+            width:80px;
+            height:80px;
+            border-radius: 50%;
+            background-color:$additional-color;
+            svg{
+                position:absolute;
+                top:50%;
+                left:50%;
+                transform:translate(-50%, -50%);
+                font-size:40px;
+                color:$primary-color;
+            }
+        }
+        h3{
+            margin-bottom:10px;
+            font-size:36px;
+            font-family: 'Roboto', sans-serif;
+            font-weight: 700;
+            color:$secondary-color;
+        }
+        p{
+            font-family: 'Raleway', sans-serif;
+            font-size:16px;
+            font-weight: 400;
+        }
+    }
+    .stats-card:hover{
+        transform: scale(1.05)
+    }
+    @media only screen and (max-width:1024px){
+        .stats-card{
+            width:320px;
+            margin-bottom:20px;
+        }
+    }
+</style>
