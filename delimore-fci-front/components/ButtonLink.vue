e6f6f12da3e276e9517c7dc2cf0fdce317bc4640
Pabeloszh <Pabeloszh@gmail.com>
2021-09-29 13:27:09 +0200

    link button added

---
+++ delimore-fci-front/components/ButtonLink.vue
@@ -0,0 +1,26 @@
+<template>
+    <NuxtLink :to="to">
+        {{value}}
+    </NuxtLink>
+</template>
+
+<script>
+    export default {
+        props: ['value', 'to'],
+    }
+</script>
+
+<style lang="scss" scoped>
+    a{
+        padding:16px 27px;
+        background-color: $additional-color;
+        font-size:16px;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 700;
+        text-decoration: none;
+        color:$primary-color;
+    }
+    a:hover{
+        background-color: $additional-hover-color;
+    }
+</style>
