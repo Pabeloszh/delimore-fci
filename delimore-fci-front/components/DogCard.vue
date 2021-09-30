dca2140df5539a9f3158c5dcbddf62fbc060fa14
Pabeloszh <Pabeloszh@gmail.com>
2021-09-30 16:09:30 +0200

    latest dogs section with card added

---
+++ delimore-fci-front/components/DogCard.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="dog-card">
+        <img src="http://localhost:1337/uploads/IMG_6351_c660ba701c.JPG" alt="">
+        <div class="desc">
+            <h4>Bombadee One Chance aka Melissa </h4>
+            <div class="birth">
+                <p>Birth year:</p>
+                <p>2019</p>
+            </div>
+            <div class="gender">
+                <p>Gender</p>
+                <p>Female</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .dog-card{
+        width:330px;
+        padding:20px;
+        transition: transform 150ms linear;
+        box-shadow: 0px 11px 15px 4px #00000040;
+        background: $primary-color;
+        cursor:pointer;
+        img{
+            width:100%;
+            height:230px;
+            object-fit: cover;
+        }
+        .desc{
+            h4{
+                margin:10px 0 15px;
+                width: 230px;
+                font-size: 24px;
+                font-family: 'DM Serif Display', serif;
+                font-weight: 400;
+                color: $secondary-color
+            }
+            div{
+                display:flex;
+                justify-content: space-between;
+                align-items: center;
+                p{
+                    font-size:16px;
+                    font-family: 'Raleway', sans-serif;
+                    font-weight: 500;
+                }
+                p:last-of-type{
+                    color:$additional-color;
+                }
+            }
+            div:first-of-type{
+                margin-bottom:10px;
+                p:last-of-type{
+                    font-family: 'Roboto', sans-serif;
+                    font-weight: 400;
+                }
+            }
+        }
+    }
+    .dog-card:hover{
+        transform: scale(1.05)
+    }
+    @media only screen and (max-width: 1024px){
+        .dog-card{
+            width:280px;
+        }
+    }
+</style>
---
+++ delimore-fci-front/components/Latest.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="latest">
+        <Title :section="'dogs'" :title="'Latest Dogs'"/>
+        <div class="cards-container">
+            <DogCard/>
+            <DogCard/>
+            <DogCard/>
+            <DogCard/>
+            <DogCard/>
+            <DogCard/>
+        </div>
+        <ButtonLink :value="'See More'" :to="'/females'"/>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .latest{
+        position:relative;
+        margin: 0 auto;
+        width:100%;
+        max-width:1024px;
+        padding: 80px 0;
+        .cards-container{
+            display:grid;
+            grid-template-columns: repeat(3, calc(33.3% - 15px));
+            grid-gap:20px;
+            margin-bottom:45px;
+        }
+        a{
+            position:absolute;
+            left:50%;
+            transform: translateX(-50%)
+        }
+    }
+    @media only screen and (max-width:1024px){
+        .latest{
+            padding:80px 25px;
+            .cards-container{
+                margin: 0 auto 45px;
+                width:280px;
+                grid-template-columns: repeat(1, 280px);
+            }
+        }
+    }
+</style>
